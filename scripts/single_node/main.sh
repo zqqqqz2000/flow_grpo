4db8cbdbfcc621322c450eb7141826f525d4bce1
Jie Liu <jieliu@link.cuhk.edu.hk>
2025-06-16 17:53:29 +0800

    Update main.sh

--- scripts/single_node/main.sh
+++ scripts/single_node/main.sh
@@ -1,4 +1,9 @@
 # 1 GPU
-accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=1 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:general_ocr_sd3
-# # 8 GPU
+# pickscore
+accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=1 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:pickscore_sd3
+# ocr
+# accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=1 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:general_ocr_sd3
+# geneval
+# accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=1 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:geneval_sd3
+# 8 GPU
 # accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=8 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:general_ocr_sd3
